90b449fd7fc5bd0c9eac56bd3fcf51818803e98d
KelechiUkogu <kelechiukogu@gmail.com>
2025-10-02 13:35:07 +0100

    App.java

--- src/main/java/pck1/0/App.java
+++ src/main/java/pck1/0/App.java
@@ -6,10 +6,11 @@ package pck1.0;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    }
+    };
     /*
    The closing semi-colon here is added
     */
-};
+}
+
 
 
